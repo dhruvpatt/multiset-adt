8ac194a0337bd2a86e31ff883b714f1ab4938ed2
armaan-n <armaannanji@gmail.com>
2023-09-18 16:34:32 -0400

    implemented MultiSet interface

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,12 @@
 public class Tree {
+
+}
+
+interface MultiSet {
+    public boolean add(Object o);
+    public void remove(Object o);
+    public boolean contains(Object o);
+    public boolean is_empty();
+    public int count(Object o);
+    public int size();
 }
